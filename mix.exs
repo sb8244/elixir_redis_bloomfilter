4068ce70710251182d84130feaafcf8d2efdf329
Stephen Bussey <sb8244@cs.ship.edu>
2017-05-29 17:06:30 -0400

    get ready for hex

--- mix.exs
+++ mix.exs
@@ -7,17 +7,37 @@ defmodule RedisBloomfilter.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     description: description(),
+     package: package(),
+     deps: deps(),
+     name: "Redis Bloomfilter",
+     source_url: "https://github.com/sb8244/elixir_redis_bloomfilter"]
   end
 
   def application do
-    [extra_applications: [:logger]]
+    []
   end
 
   defp deps do
     [
       {:redix, ">= 0.0.0", only: :test},
       {:mock, "~> 0.2.0", only: :test},
+    ]
+  end
+
+  defp description do
+    """
+    A distributed bloom filter implementation based on Redis. Uses Erik Dubbelboer's LUA scripts for the bloom filter implementation.
+    """
+  end
+
+  defp package do
+    [
+      name: :redis_bloomfilter,
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Stephen Bussey"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/sb8244/elixir_redis_bloomfilter"}
     ]
   end
 end
